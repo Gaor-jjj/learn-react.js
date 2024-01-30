6dba2fba4b633045e1f0acb430ef319dc7389898
Gaor-jjj <kaurtamm1995@gmail.com>
2024-01-30 15:20:46 +0200

    Apply Card component to ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,11 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
+import Card from './Card'
 
 function ExpenseItem(props){
 
     return (
-        <div className='expense-item'>
+        <Card className='expense-item'>
             <ExpenseDate
                 date={props.expenseData.date}
             />  
@@ -12,7 +13,7 @@ function ExpenseItem(props){
                 <h2>{props.expenseData.title}</h2>
                 <div className='expense-item__price'>{props.expenseData.price}</div>
             </div>
-        </div>
+        </Card>
     )
 }
 
